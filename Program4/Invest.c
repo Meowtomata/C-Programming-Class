7e31be143ce5a4a15fd700d4ff3b8bf3aa6c60fc
Meowtomata <its.anatoliy@gmail.com>
2024-02-13 19:44:54 -0500

    Implement Invest.c

--- Program4/Invest.c
+++ Program4/Invest.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 void minMaxAvg(double*, int);
+void bestCase(double*, int);
 
 int main(void) 
 {
@@ -18,6 +19,7 @@ int main(void)
     double stocks[numOfDays];
     
     minMaxAvg(stocks, numOfDays);
+    bestCase(stocks, numOfDays);
 
     return 0;
 }
@@ -56,4 +58,43 @@ void minMaxAvg(double* stocks, int numOfDays)
     printf("Min price $%.2lf on day %i\n", min, minDay);
     printf("Max price $%.2lf on day %i\n", max, maxDay);
     printf("Average price $%.2lf\n", sum / numOfDays);
+}
+
+void bestCase(double* stocks, int numOfDays) 
+{
+    int simulatedTradeCount = 0;
+    double maxRelativeChange = 0;
+    double minRelativeChange = __DBL_MAX__;
+    double relativeChangeSum = 0;
+
+    int bestBuyDay = 0;
+    int bestSellDay = 0;
+    int worstSellDay = 0;
+    int worstBuyDay = 0;
+    for (int i = 0; i < numOfDays; i++) 
+    {
+        for (int j = i + 1; j < numOfDays; j++) 
+        {
+            double relativeChange = ((stocks[j] - stocks[i]) / stocks[i]) * 100;
+            if (relativeChange > maxRelativeChange) 
+            {
+                maxRelativeChange = relativeChange;
+                bestBuyDay = i + 1;
+                bestSellDay = j + 1;
+            }
+            if (relativeChange < minRelativeChange) 
+            {
+                minRelativeChange = relativeChange;
+                bestBuyDay = i + 1;
+                bestSellDay = j + 1;
+            }
+            relativeChangeSum += relativeChange;
+            simulatedTradeCount++;
+        }
+    }
+
+    printf("Simulated %i possible trades\n", simulatedTradeCount);
+    printf("Average case, %.2f%%\n", relativeChangeSum / simulatedTradeCount);
+    printf("Best case, buy day %i, sell day %i, %.2f%%\n", bestBuyDay, bestSellDay, maxRelativeChange);
+    printf("Worst case, buy day %i, sell day %i, %.2f%%\n", worstBuyDay, worstSellDay, minRelativeChange);
 }
