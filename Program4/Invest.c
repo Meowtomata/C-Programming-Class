13d38fbf664015b3901b2f16e01a8f763a768f6a
Meowtomata <its.anatoliy@gmail.com>
2024-02-18 08:45:04 -0500

    Comment changes

--- Program4/Invest.c
+++ Program4/Invest.c
@@ -23,12 +23,22 @@ int main(void)
 
     double stocks[numOfDays];
     
-    if (minMaxAvg(stocks, numOfDays) == 1) return 1;
-    bestCase(stocks, numOfDays);
+    // minMaxAvg returns the number of Stocks initialized in array
+    int numStocks = minMaxAvg(stocks, numOfDays);
+    // minmaxAvg returns -1 if a stock price is 0 or negative
+    if (numStocks == -1) return 1;
+
+    bestCase(stocks, numStocks);
 
     return 0;
 }
 
+/*
+    Calculate and otuput the min, average, and max stock price
+    Populate the stocks array
+    Return the number of stocks populated in array
+    Return -1 if error
+*/
 int minMaxAvg(double* stocks, int numOfDays) 
 {
     double min = __DBL_MAX__; 
@@ -39,37 +49,43 @@ int minMaxAvg(double* stocks, int numOfDays)
     double sum = 0;
     int i = 0;
 
-    // && i <= numOfDays ??
-    while (scanf("%lf", &stocks[i]) > 0) 
+    // scan until blank input or specified number of days reached
+    while (i < numOfDays && scanf("%lf", &stocks[i]) > 0) 
     {
         if (stocks[i] <= 0) 
         {
             printf("ERROR: prices must be positive!\n");
-            return 1;
+            return -1;
         }
+        // find minimum stock price and day
         if (stocks[i] < min) 
         {
             min = stocks[i];
             minDay = i + 1;
         }
+        // find maximum stock price and day
         if (stocks[i] > max) 
         {
             max = stocks[i];
             maxDay = i + 1;
         }
         sum += stocks[i];
-        i+=1;
+        i += 1;
     }
 
     printf("Read %i days\n", numOfDays);
     printf("Min price $%.2lf on day %i\n", min, minDay);
     printf("Max price $%.2lf on day %i\n", max, maxDay);
-    printf("Avg price $%.2lf\n", sum / numOfDays);
+    printf("Avg price $%.2lf\n", sum / i);
 
-    return 0;
+    return i;
 }
 
-void bestCase(double* stocks, int numOfDays) 
+/*
+    Calculate and output the best and worst buy and sell days
+    Also find and output the average relative change between buy and sell days
+*/
+void bestCase(double* stocks, int numStocks) 
 {
     int simulatedTradeCount = 0;
     double maxRelativeChange = 0;
@@ -80,17 +96,20 @@ void bestCase(double* stocks, int numOfDays)
     int bestSellDay = 0;
     int worstSellDay = 0;
     int worstBuyDay = 0;
-    for (int i = 0; i < numOfDays; i++) 
+    // starting at some buy day, go through each proceeding stock to find sell day
+    for (int i = 0; i < numStocks; i++) 
     {
-        for (int j = i + 1; j < numOfDays; j++) 
+        for (int j = i + 1; j < numStocks; j++) 
         {
             double relativeChange = ((stocks[j] - stocks[i]) / stocks[i]) * 100;
+            // find best buy and sell day
             if (relativeChange > maxRelativeChange) 
             {
                 maxRelativeChange = relativeChange;
                 bestBuyDay = i + 1;
                 bestSellDay = j + 1;
             }
+            // find worst buy and sell day
             if (relativeChange < minRelativeChange) 
             {
                 minRelativeChange = relativeChange;
